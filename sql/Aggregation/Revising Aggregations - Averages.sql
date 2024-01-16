ac2eeac59e98c1a827901074c00da285778baf23
Jomaica Lei <jalfiler@seattleu.edu>
2024-01-16 12:23:47 -0800

    Create Revising Aggregations - Averages.sql

---
+++ sql/Aggregation/Revising Aggregations - Averages.sql
@@ -0,0 +1,7 @@
+--Problem: https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/submissions/code/365187294
+--Score: 10
+
+select 
+    avg(Population)
+from City
+where District = 'California';

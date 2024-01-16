395a0f92c5f791b227e47cf67f54e12b0ee19596
Jomaica Lei <jalfiler@seattleu.edu>
2024-01-16 00:37:56 -0800

    Delete LeetCode directory

--- LeetCode/SQL50/Easy/1757. Recyclable and Low Fat Products.sql
+++
@@ -1,9 +0,0 @@
---https://leetcode.com/problems/recyclable-and-low-fat-products/submissions/1147651318?envType=study-plan-v2&envId=top-sql-50
-
---Runtime: 949 ms
-
-# Write your MySQL query statement below
-select 
-    product_id
-from Products
-where low_fats = 'Y' and recyclable = 'Y';
